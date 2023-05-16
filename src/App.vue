668ffdb6d88cfa18e9c947c287c451505d05865d
duntecityzen <duncanochieng682@gmail.com>
2023-05-16 19:28:20 +0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <Navbar />
+  <Intro />
+  <About />
+  <JobsIntro />
+  <Contact />
+  <Footer />
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+import Intro from './components/Intro.vue'
+import About from './components/About.vue'
+import JobsIntro from './components/JobsIntro.vue'
+import Contact from './components/Contact.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    Intro,
+    About,
+    JobsIntro,
+    Contact,
+    Footer
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/simplelightbox/dist/simple-lightbox.min.css'
+import './assets/theme.js'
+import './assets/theme.css'
+
+createApp(App).mount('#app')
