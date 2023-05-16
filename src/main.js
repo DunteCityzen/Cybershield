5edb70322d2e00c26faabb761f20a55373ddfe96
duntecityzen <duncanochieng682@gmail.com>
2023-05-17 02:45:22 +0300

    The second commit

--- src/main.js
+++ src/main.js
@@ -4,5 +4,8 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/simplelightbox/dist/simple-lightbox.min.css'
 import './assets/theme.js'
 import './assets/theme.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+import '../node_modules/simplelightbox/dist/simple-lightbox.min.js'
+
 
 createApp(App).mount('#app')
